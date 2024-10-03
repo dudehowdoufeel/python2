2fbec893dc7871180b516f981ffa2110ad288386
dudehowdoufeel <156789719+dudehowdoufeel@users.noreply.github.com>
2024-10-04 01:21:01 +0500

    Delete quiz directory

--- quiz/list.py
+++
@@ -1,6 +0,0 @@
-def remove_duplicates(elements):
-    return list(set(elements))
-
-input_list = ['a', 'a', 'e', 'o', 'p', 'i', 'e', 'j']
-unique_list = remove_duplicates(input_list)
-print(f"Список без дубликатов: {unique_list}")
--- quiz/odd.py
+++
@@ -1,3 +0,0 @@
-numbers=[1,2,3,4,5,6,7,8,9]
-odd=[num for num in numbers if num%2==1]
-print(odd)
